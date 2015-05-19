1cb6df800f56249193845bdb4b55689ee3bf199a
Edmund Lam <edmund@Sigma.Awe.Rig>
2015-05-19 15:38:59 +0300

    Fixed Calendar pretty printer

--- comingle_runtime/refined/src/CoMingleRuntimeRefined/src/comingle/pretty/PrettyPrinter.java
+++ comingle_runtime/refined/src/CoMingleRuntimeRefined/src/comingle/pretty/PrettyPrinter.java
@@ -3,12 +3,18 @@ package comingle.pretty;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class PrettyPrinter {
 
 	public static String pretty(Calendar cal) {
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		return df.format( cal.getTime() );
+	}
+	
+	public static String prettyTime(long date) {
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return df.format(new Date(date));
 	}
 	
 }
